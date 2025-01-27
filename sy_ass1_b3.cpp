460a32e3fe44795bb5fbd4ec39cd297702c0abac
Pratiksha Avhad <pratikshaavhad91@gmail.com>
2025-01-28 00:00:03 +0530

    Add files via upload

---
+++ sy_ass1_b3.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	int n,i,s=0;
+	cout<<"enter limit";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		cout<<i*i<<"+";
+		s=s+(i*i);
+	}
+	cout<<"\b="<<s;
+}

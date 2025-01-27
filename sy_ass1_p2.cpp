7444b1fb05881f21e9a3a12466e8e19228e1c03f
Pratiksha Avhad <pratikshaavhad91@gmail.com>
2025-01-28 00:02:59 +0530

    Add files via upload

---
+++ sy_ass1_p2.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	int n,i;
+	cout<<"enter number";
+	cin>>n;
+	if(n%2==0)
+	  cout<<"number is even";
+	else
+	  cout<<"number is odd";   
+}

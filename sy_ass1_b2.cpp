c4a7af5e24312c11c6d674054f455ddcd7241b94
Pratiksha Avhad <pratikshaavhad91@gmail.com>
2025-02-17 20:38:27 +0530

    Add files via upload

---
+++ sy_ass1_b2.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	int i,j;
+	for(i=1;i<=4;i++)
+	{
+		for(j=4;j>=i;j--)
+		{
+			cout<<" ";
+		}
+		for(j=1;j<=i;j++)
+		{
+			cout<<"*";
+		}
+		cout<<"\n";
+	}
+}
+
+
